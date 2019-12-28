22ab561fac2b5cece0e6dc843abfeadd02013e23
Yuliia Engman <yuliia.engman@gmail.com>
2019-12-28 00:25:06 -0500

    Initial Commit

---
+++ Quebec2/DisplayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quebec2
+//
+//  Created by Yuliia Engman on 12/27/19.
+//  Copyright © 2019 Yuliia Engman. All rights reserved.
+//
+
+import UIKit
+
+class DisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
